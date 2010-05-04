0748982ca1fba3c343f12aa48df22afb23d284aa
robertkrimen <robertkrimen@gmail.com>
2010-05-03 19:53:09 -0700

    Conversion to using Dist::Dzpl

--- lib/Google/Search.pm
+++ lib/Google/Search.pm
@@ -1,19 +1,9 @@
 package Google::Search;
+# ABSTRACT: Interface to the Google AJAX Search API and suggestion API
+
 
 use warnings;
 use strict;
-
-=head1 NAME
-
-Google::Search - Interface to the Google AJAX Search API and suggestion API
-
-=head1 VERSION
-
-Version 0.025
-
-=cut
-
-our $VERSION = '0.025';
 
 =head1 SYNOPSIS
 
@@ -525,65 +515,4 @@ The passing order of the array, hash, and string does not matter
 
 =cut
 
-
-=head1 AUTHOR
-
-Robert Krimen, C<< <rkrimen at cpan.org> >>
-
-=head1 SEE ALSO
-
-L<REST::Google::Search>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-google-search at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Google-Search>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Google::Search
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Google-Search>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Google-Search>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Google-Search>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Google-Search>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 Robert Krimen, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of Google::Search
+1;
